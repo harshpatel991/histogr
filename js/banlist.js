847cc7486fbdd395b4b8d4ffb519cb2406b9bae1
Harsh <hpatel90@illinos.edu>
2015-04-25 23:32:04 -0500

    Cleaning up banlist, recording times for restricting domains

--- js/banlist.js
+++ js/banlist.js
@@ -1,3 +1,6 @@
+//Regular expression for time validation taken
+// courtesy of http://www.the-art-of-web.com/javascript/validate-date/
+
 //On clicking the banlist side bar button, refresh the lists
 $("#banlist-button").click(function() {
     $('#mainTabList a[href="#banlist"]').tab('show')
@@ -10,21 +13,53 @@ $("#submit-distracting").click(addDistractingDomainFromTextBox);
 
 //Called when clicking restricted domain button
 function addRestrictedDomain() {
+    $('#banlist-error-box').html("");//clear out error box
+
     var urlToBlock = $('#restricted-domain').val();
+    var timeFrom = $('#restricted-domain-from').val();
+    var timeTo = $('#restricted-domain-to').val();
 
-    $('#restricted-domain').val(""); //clear the box
+    var timeRegExp = /^\d{1,2}:\d{2}([ap]m)?$/;
 
-    //Add to storage and refresh the list of domains
-    appendToStorage("restrictedDomains", urlToBlock, function () {
-        listRestrictedDomains();
-    });
+    if(urlToBlock != '' && timeFrom != '' && timeFrom.match(timeRegExp) && timeTo != '' && timeTo.match(timeRegExp)) {
+
+        $('#restricted-domain').val(""); //clear the boxes
+        $('#restricted-domain-from').val("");
+        $('#restricted-domain-to').val("");
+
+        //Add to storage and afterwards refresh the list of domains
+        appendToStorage("restrictedDomains", urlToBlock, function () {
+            appendToStorage("restrictedDomainsTimeFrom", timeFrom, function () {
+                appendToStorage("restrictedDomainsTimeTo", timeTo, function () {
+                    console.log("saved: " + timeFrom + timeTo);
+                    listRestrictedDomains();
+                });
+            });
+        });
+    }
+    else if (urlToBlock === '') {
+        $('#banlist-error-box').html('<div class="alert alert-danger" role="alert">Please enter a domain</div>');
+    }
+    else {
+        console.log("error!");
+        $('#banlist-error-box').html('<div class="alert alert-danger" role="alert">Please enter valid times (ex 1:45pm)</div>');
+    }
+
 }
 
 //Called when clicking distracting domain button
 function addDistractingDomainFromTextBox() {
+    $('#banlist-error-box').html("");//clear out error box
+
     var urlMarkAsDistracting = $('#distracting-domain').val();
-    $('#distracting-domain').val(""); //clear the box
-    addDistractingDomain(urlMarkAsDistracting);
+
+    if(urlMarkAsDistracting != '') {
+        $('#distracting-domain').val(""); //clear the box
+        addDistractingDomain(urlMarkAsDistracting);
+    }
+    else {
+        $('#banlist-error-box').html('<div class="alert alert-danger" role="alert">Please enter a domain</div>');
+    }
 }
 
 function addDistractingDomain(domain) {
@@ -39,13 +74,19 @@ function listRestrictedDomains() {
     $("#restricted-domains-list").empty(); //clear any domains on the page
 
     retrieveFromStorage("restrictedDomains", function(items) { //get domains from storage
-        console.log(items);
-        if(items !== undefined) {
-            for (var index = 0; index < items.length; index++) {
-                $("#restricted-domains-list").append(items[index] + '<button id="delete-restricted-' + index +'">Delete</button><br>');
-                $("#delete-restricted-"+index).click(deleteRestrictedDomain(index));
-            }
-        }
+        retrieveFromStorage("restrictedDomainsTimeFrom", function(restrictedFrom) { //get timeFrom from storage
+            console.log("got from storage restrictedFrom: " + restrictedFrom);
+            retrieveFromStorage("restrictedDomainsTimeTo", function(restrictedTo) { //get timeTo from storage
+
+                console.log(items);
+                if(items !== undefined) {
+                    for (var index = 0; index < items.length; index++) {
+                        $("#restricted-domains-list").append(items[index] + ' <b>From: </b>' + restrictedFrom[index] + ' <b>To:</b> ' + restrictedTo[index] +'<button class="btn btn-danger pull-right" id="delete-restricted-' + index +'"><span class="glyphicon glyphicon-trash"></span> Delete</button><br><hr>');
+                        $("#delete-restricted-"+index).click(deleteRestrictedDomain(index));
+                    }
+                }
+            });
+        });
     });
 }
 
@@ -55,7 +96,7 @@ function listDistractingDomains() {
     retrieveFromStorage("distractingDomains", function(items) { //get domains from storage
         if(items !== undefined) {
             for (var index = 0; index < items.length; index++) {
-                $("#distracting-domains-list").append(items[index] + '<button id="delete-distracting-' + index + '">Delete</button><br>');
+                $("#distracting-domains-list").append(items[index] + '<button class="btn btn-danger pull-right" id="delete-distracting-' + index + '"><span class="glyphicon glyphicon-trash"></span> Delete</button><br><hr>');
                 $("#delete-distracting-" + index).click(deleteDistractingDomain(index));
             }
         }
@@ -64,14 +105,18 @@ function listDistractingDomains() {
 
 function deleteRestrictedDomain(index) {
     return function () {
-        console.log("deleting restricted index: " + index);
-        pruneFromStorage("restrictedDomains", index, function() {listRestrictedDomains();});
+        pruneFromStorage("restrictedDomains", index, function() {
+            pruneFromStorage("restrictedDomainsTimeFrom", index, function() {
+                pruneFromStorage("restrictedDomainsTimeTo", index, function() {
+                        listRestrictedDomains();
+                });
+            });
+        });
     }
 }
 
 function deleteDistractingDomain(index) {
     return function () {
-        console.log("deleting distracting index: " + index);
         pruneFromStorage("distractingDomains", index, function() {listDistractingDomains();});
     }
 }
