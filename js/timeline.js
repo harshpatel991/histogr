35dab176bccae3c93059ed6be8cad776a2ca04fb
rosario-luke <lrosari2@illinois.edu>
2015-04-27 21:20:14 -0500

    import rickshaw and cleared timeline

--- js/timeline.js
+++ js/timeline.js
@@ -5,16 +5,5 @@ $("#timeline-button").click(function() {
 });
 
 
-var data = [1, 2, 3, 5, 6];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
-
-d3.select(".timeline-chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
 
